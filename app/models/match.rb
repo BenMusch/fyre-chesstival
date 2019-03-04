0d1b08915f41af66d347a9fade56fb2feda291c4
Ben Muschol <benmuschol@gmail.com>
2019-03-03 19:11:28 -0500

    Customize matches

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,4 +1,5 @@
 class Match < ApplicationRecord
-  belongs_to :white
-  belongs_to :black
+  belongs_to :white, class_name: "Player"
+  belongs_to :black, class_name: "Player"
+  enum result: [ :incomplete, :white_wins, :black_wins, :draw, :all_lose ]
 end
--- db/migrate/20190304000904_create_matches.rb
+++ db/migrate/20190304000904_create_matches.rb
@@ -1,9 +1,9 @@
 class CreateMatches < ActiveRecord::Migration[5.2]
   def change
     create_table :matches do |t|
-      t.references :white, foreign_key: true
-      t.references :black, foreign_key: true
-      t.integer :result
+      t.references :white, foreign_key: { to_table: :players }
+      t.references :black, foreign_key: { to_table: :players }
+      t.integer :result, default: 0
       t.integer :round_number
 
       t.timestamps
